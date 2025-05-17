c5cc58f8c6eb5b6c5b405770fbb2648ae9d472ea
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-05-17 23:56:28 +0900

    go to the result page

--- src/main/java/org/example/demo/ScoreInputController.java
+++ src/main/java/org/example/demo/ScoreInputController.java
@@ -1,11 +1,19 @@
 package org.example.demo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ScoreInputController {
     @FXML private VBox rootBox;
@@ -24,7 +32,13 @@ public class ScoreInputController {
             rootBox.getChildren().addAll(label, field);
         }
     }
-
+    @FXML
+    private void handleResult(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("result.fxml")));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root, 600, 400));
+        stage.setTitle("Result");
+    }
     @FXML
     private void saveScores() {
         List<GpaForm.Subject> subjects = GpaForm.subjectList;

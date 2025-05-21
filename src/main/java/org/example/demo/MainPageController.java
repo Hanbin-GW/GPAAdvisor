edf0da7f545f68e9ec6cb33e30fef1972af79431
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-05-21 13:54:02 +0900

    Translation in english

--- src/main/java/org/example/demo/MainPageController.java
+++ src/main/java/org/example/demo/MainPageController.java
@@ -6,11 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class MainPageController extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Hello World");
-        Parent root = FXMLLoader.load(getClass().getResource("main-page.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main-page.fxml")));
         Scene scene = new Scene(root, 600, 400);
         stage.setTitle("GPA Advisor");
         stage.setScene(scene);

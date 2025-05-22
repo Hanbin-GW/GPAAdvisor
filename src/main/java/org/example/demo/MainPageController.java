eae4fe90f62be5c9f73b1d3dd932b2c7162f76b6
Hanbin-GW <hanbin.scpsl@gmail.com>
2025-05-22 11:09:37 +0900

    rework in progress...

--- src/main/java/org/example/demo/MainPageController.java
+++ src/main/java/org/example/demo/MainPageController.java
@@ -18,4 +18,28 @@ public class MainPageController extends Application {
         stage.setScene(scene);
         stage.show();
     }
+
+    private static class Subject{
+        private String name;
+        private double homework;
+        private double quiz;
+        private double test;
+        private double exam;
+        private double credits;
+        private double finalScore; // FinalScore (Max Score 100)
+        private String letterGrade; // Credit (A+, A0 , etc)
+        private double gpaValue; // GPA (4.5, 4.3 ,etc)
+
+        public Subject(String name, double credits) {
+            this.name = name;
+            this.credits = credits;
+            this.homework = 0;
+            this.quiz = 0;
+            this.test = 0;
+            this.exam = 0;
+            this.finalScore = 0;
+            this.letterGrade = "F";
+            this.gpaValue = 0.0;
+        }
+    }
 }
